2a648910ce5cb4bc295686bbda2fa8226706db5b
Nishank Soni <soninishank8@gmail.com>
2022-08-27 16:40:15 -0700

    Time: 3 ms (98.79%), Space: 42.3 MB (96.44%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,4 +1,9 @@
 class Solution {
+
+// Time complexity : O(2n) = O(n)O(2n)=O(n). In the worst case each character will be visited twice by ii and jj.
+// Space complexity : O(min(m, n))O(min(m,n)). Same as the previous approach. We need O(k) space for the sliding window, where k is the size of the Set. The size of the Set is upper bounded by the size of the string nn and the size of the charset/alphabet mm.
+    
+    
     public int lengthOfLongestSubstring(String s) {
         int[] arr = new int[256];
         int end = 0 , start = 0;
