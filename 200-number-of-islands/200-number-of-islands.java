ae5a75a9417bab5ab4a25fba6f7c4704a34f816f
Nishank Soni <soninishank8@gmail.com>
2022-08-27 05:00:43 -0700

    Time: 3 ms (97.74%), Space: 50.8 MB (92.24%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,29 +1,30 @@
 class Solution {
-    int[][] direction = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+    int[][] dir = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 
     public int numIslands(char[][] grid) {
         int count = 0;
-        Queue<int[]> queue = new LinkedList<>();
+        char islandColor = '1';
+        char maskColor = '*';
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '1') {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == islandColor) {
                     count++;
-                    queue.add(new int[] { i, j });
-                    grid[i][j] = '0';
-                    while (!queue.isEmpty()) {
-                        int[] arr = queue.poll();
-                        for (int[] way : direction) {
-                            int x = way[0] + arr[0];
-                            int y = way[1] + arr[1];
-                            if (x >= 0 && y >= 0 && x <= grid.length - 1 && y <= grid[0].length - 1 && grid[x][y] == '1') {
-                                queue.add(new int[] { x, y });
-                                grid[x][y] = '0';
-                            }
-                        }
-                    }
+                    applyDfs(i, j, grid, islandColor, maskColor);
                 }
             }
         }
         return count;
+    }
+
+    private void applyDfs(int row, int col, char[][] grid, char islandColor, char maskColor) {
+        if (row >= 0 && col >= 0 && row < grid.length && col < grid[0].length && grid[row][col] == islandColor) {
+            grid[row][col] = maskColor;
+            applyDfs(row, col - 1, grid, islandColor, maskColor);
+            applyDfs(row, col + 1, grid, islandColor, maskColor);
+            applyDfs(row - 1, col, grid, islandColor, maskColor);
+            applyDfs(row + 1, col, grid, islandColor, maskColor);
+        } else {
+            return;
+        }
     }
 }
