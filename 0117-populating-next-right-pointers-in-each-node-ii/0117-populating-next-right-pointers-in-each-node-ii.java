2bdf4a87871ca3f70138c6436688b17a4cde9722
Nishank Soni <soninishank8@gmail.com>
2022-11-13 16:47:46 -0800

    Time: 1 ms (88.21%), Space: 41.8 MB (95.00%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,52 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    // https://practice.geeksforgeeks.org/problems/connect-nodes-at-same-level/1
+    public Node connect(Node root) {
+        if(root == null){
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty())
+        {
+            int size = queue.size();
+            Node dummy = new Node(0);
+            for(int i = 0 ; i < size ; i++)
+            {
+                Node pollVal = queue.poll();
+                dummy.next = pollVal;
+                dummy = dummy.next;
+                if(pollVal.left != null)
+                {
+                    queue.add(pollVal.left);
+                }
+                if(pollVal.right != null){
+                    queue.add(pollVal.right);
+                }
+            }
+        }
+        return root;
+    }
+}
