f02afc9574f37e81fa351e4a0d813c76e4094764
Nishank Soni <soninishank8@gmail.com>
2023-12-02 15:39:03 -0800

    Time: 30 ms (51.20%), Space: 46.3 MB (94.25%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,25 +1,24 @@
 class Solution {
+
     public int[][] kClosest(int[][] points, int k) {
-        HashMap<int[], Integer> hashmap = new HashMap<>();
-        for(int[] arr: points){
-            hashmap.put(arr,findEuclideanDistance(arr));
-        }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> (hashmap.get(b) - hashmap.get(a)));
-        pq.addAll(hashmap.keySet());
-        while(pq.size() > k){
-            pq.poll();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> findEuclideanDistance(b) - findEuclideanDistance(a));
+        for (int point[] : points) {
+            pq.add(point);
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
         int[][] arr = new int[pq.size()][2];
         int i = 0;
-        while(!pq.isEmpty()){
+        while (!pq.isEmpty()) {
             arr[i] = pq.poll();
             i++;
         }
         return arr;
     }
-    
-    public int findEuclideanDistance(int[] arr){
-        int x1 = arr[0]- 0;
+
+    public int findEuclideanDistance(int[] arr) {
+        int x1 = arr[0] - 0;
         int x2 = arr[1] - 0;
         return x1 * x1 + x2 * x2;
     }
