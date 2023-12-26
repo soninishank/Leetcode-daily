10f6d9eb15be4bf98c1e11e9828ef3029b869a5d
Nishank Soni <soninishank8@gmail.com>
2023-12-26 15:35:11 -0800

    Time: 618 ms (5.04%), Space: 45.6 MB (7.49%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,30 +1,29 @@
 class Solution {
-   public String minRemoveToMakeValid(String s) {
-        char[] ch = s.toCharArray();
+    public String minRemoveToMakeValid(String s) {
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < ch.length; i++) {
-            if (s.charAt(i) == '(') {
+        List<Integer> invalidChars = new ArrayList<>();
+        char[] ch = s.toCharArray();
+        for(int i = 0; i < s.length(); i++){
+            if(ch[i] == '('){
                 stack.push(i);
-            }
-            if (s.charAt(i) == ')') {
-                if (!stack.isEmpty() && ch[stack.peek()] == '(') {
+            }else if(ch[i]==')'){
+                if(stack.isEmpty()){
+                    invalidChars.add(i);
+                }else if(ch[stack.peek()] == '('){
                     stack.pop();
-                } else {
-                    stack.push(i);
                 }
             }
         }
-        if (stack.size() == ch.length) {
-            return "";
+        // "))(("
+        while(!stack.isEmpty()){
+            invalidChars.add(stack.pop());
         }
         StringBuilder builder = new StringBuilder();
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (!stack.isEmpty() && i == stack.peek()) {
-                stack.pop();
-            } else {
-                builder.append(s.charAt(i));
+        for(int i = 0 ; i < ch.length; i++){
+            if(!invalidChars.contains(i)){
+             builder.append(ch[i]);   
             }
         }
-        return builder.reverse().toString();
+        return builder.toString();
     }
 }
