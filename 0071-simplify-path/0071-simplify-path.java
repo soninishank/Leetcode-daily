220884614113db6e98fc80c2202820df33970599
Nishank Soni <soninishank8@gmail.com>
2024-01-31 19:55:57 -0800

    Time: 4 ms (86.33%), Space: 43.2 MB (74.77%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,28 @@
+class Solution {
+    Stack<String> stack = new Stack<>();
+
+    public String simplifyPath(String path) {
+        StringBuilder builder = new StringBuilder();
+        String[] strList = path.split("/");
+        for (String str : strList) {
+            if (str.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (str.equals(".") || str.equals("")) {
+                continue;
+            } else {
+                stack.push(str);
+            }
+        }
+        for (String str : stack) {
+            builder.append("/");
+            builder.append(str);
+        }
+        if (builder.length() > 0) {
+            return builder.toString();
+        } else {
+            return "/";
+        }
+    }
+}
