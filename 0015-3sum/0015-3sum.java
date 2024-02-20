9a2c13ef8c657bcb8b46e61eb438cd3c3b33463a
Nishank Soni <soninishank8@gmail.com>
2024-02-19 17:38:12 -0800

    Time: 32 ms (58.92%), Space: 51.2 MB (88.31%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,36 @@
 class Solution {
+    List<List<Integer>> result = new ArrayList<>();
 
     public List<List<Integer>> threeSum(int[] nums) {
+        // Base case
+        if (nums.length == 0) {
+            return result;
+        }
         Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-        int target = 0;
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int low = i + 1;
-                int high = nums.length - 1;
-                while (low < high) {
-                    int sum = nums[low] + nums[high] + nums[i];
-                    if (sum == target) {
-                        list.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                        while (low < high && nums[low] == nums[low + 1]) {
-                            low++;
-                        }
-                        while (low < high && nums[high] == nums[high - 1]) {
-                            high--;
-                        }
-                        low++;
-                        high--;
-                    } else if (sum > target) {
-                        high--;
-                    } else {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int low = i + 1, high = nums.length - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    while (low < high && nums[low] == nums[low + 1]) {
                         low++;
                     }
+                    while (low < high && nums[high] == nums[high - 1]) {
+                        high--;
+                    }
+                    low++;
+                    high--;
+                } else if (sum > 0) {
+                    high--;
+                } else {
+                    low++;
                 }
             }
         }
-        return list;
+        return result;
     }
 }
