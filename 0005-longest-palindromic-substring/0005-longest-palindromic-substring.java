83ad77d8a6a7cc1da100f9584ef7098c669f3950
Nishank Soni <soninishank8@gmail.com>
2024-02-21 15:21:18 -0800

    Time: 13 ms (94.67%), Space: 42.1 MB (82.59%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,27 @@
 class Solution {
-    // Its very easy
-    // Just think of a single character
-    // single character is always palindrome
-    // then take two characters - "ba"
     int finalDifference = 0;
     int start = 0;
 
     public String longestPalindrome(String s) {
-        if(s.isEmpty()){
+        if (s.isEmpty()) {
             return "";
         }
         for (int i = 0; i < s.length(); i++) {
-            extendAroundPalindrome(s, i, i);
-            extendAroundPalindrome(s, i, i + 1);
+            findLength(s, i, i);
+            findLength(s, i, i + 1);
         }
-
-        return s.substring(start, start + finalDifference);
+        return s.substring(start,start+finalDifference);
     }
 
-    private void extendAroundPalindrome(String str, int low, int high) {
-        while (low >= 0 && high <= str.length() - 1 && str.charAt(low) == str.charAt(high)) {
-            low--;
-            high++;
+    private void findLength(String str, int low, int high) {
+        while (low >= 0 && high < str.length()) {
+            if (str.charAt(low) == str.charAt(high)) {
+                low--;
+                high++;
+            } else {
+                break;
+            }
         }
-        // low = -1 , high = 1 so to get a single digit as Palindrome we need to subtract 1
-        // 1 - (-1) -1 
-        // 1 + 1 - 1 = 2 -1 = 1
         int diff = high - low - 1;
         if (diff > finalDifference) {
             finalDifference = diff;
