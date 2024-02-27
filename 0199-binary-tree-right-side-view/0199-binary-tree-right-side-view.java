493ff39d66fe52b1b0365d3d5c2e7b711a89bc0a
Nishank Soni <soninishank8@gmail.com>
2024-02-27 10:55:51 -0800

    Time: 0 ms (100.00%), Space: 41.9 MB (79.13%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,29 +14,25 @@
  * }
  */
 class Solution {
-    List<Integer> result = new ArrayList<>();
+    private List<Integer> result = new ArrayList<>();
 
     public List<Integer> rightSideView(TreeNode root) {
         if (root == null) {
             return result;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (i == size - 1) {
-                    result.add(node.val);
-                }
-                if (node.left != null) {
-                    queue.add(node.left);
-                }
-                if (node.right != null) {
-                    queue.add(node.right);
-                }
-            }
-        }
+        buildTree(root, 1);
         return result;
+    }
+
+    private void buildTree(TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+        if (result.size() < level) {
+            result.add(root.val);
+        }
+        // Pre-Order traversal
+        buildTree(root.right, level + 1);
+        buildTree(root.left, level + 1);
     }
 }
