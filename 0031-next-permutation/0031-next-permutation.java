e941f198830a8fc0ba36bf2c59b141c5e85a6dc1
Nishank Soni <soninishank8@gmail.com>
2024-03-02 11:49:43 -0800

    Time: 3 ms (5.71%), Space: 42.8 MB (74.08%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    public void nextPermutation(int[] nums) {
+        int pivot = findPivot(nums); 
+        System.out.println(pivot);
+        if (pivot != -1) {
+            int nextSuccessor = findNextSuccessor(nums, pivot);
+            swap(nums, pivot, nextSuccessor);
+        }
+        reverseAfterPivot(nums, pivot + 1); // 3,2,1
+    }
+
+    // R.H.S
+    private int findPivot(int[] nums) {
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
+                return i - 1;
+            }
+        }
+        return -1;
+    }
+
+    private int findNextSuccessor(int[] nums, int pivot) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > nums[pivot]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private void swap(int[] arr, int pivot, int successor) {
+        int temp = arr[pivot];
+        arr[pivot] = arr[successor];
+        arr[successor] = temp;
+    }
+
+    private void reverseAfterPivot(int[] arr, int pivotNext) {
+        int high = arr.length - 1;
+        while (pivotNext <= high) {
+            int temp = arr[pivotNext];
+            arr[pivotNext] = arr[high];
+            arr[high] = temp;
+            high--;
+            pivotNext++;
+        }
+    }
+}
