0f794826a48633d3526f8a513af7c2046c8e02c0
Nishank Soni <soninishank8@gmail.com>
2024-03-02 12:30:14 -0800

    Time: 15 ms (5.22%), Space: 41.4 MB (7.75%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -17,12 +17,16 @@ class Solution {
         divisor = Math.abs(divisor);
         int result = 0;
         
+        // temp << 1 -> left shift operator 
+        
+        
         while(dividend - divisor >= 0){
             int temp = divisor;
             int count = 1;
             while(dividend - (temp << 1) >= 0){
-                temp = temp<<1;
+                temp = temp<<1; // int temp = 3, temp = 3 * 2^1 = 3 * 2 = 6
                 count = count<<1;
+                System.out.println(temp + " "+count);
             }
             result += count;
             dividend -= temp;
