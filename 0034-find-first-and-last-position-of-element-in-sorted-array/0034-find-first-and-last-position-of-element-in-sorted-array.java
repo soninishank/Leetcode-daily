dd2ffe38d80f01a2b65a113ecd23e7874d9a5fee
Nishank Soni <soninishank8@gmail.com>
2024-03-03 08:04:39 -0800

    Time: 0 ms (100.00%), Space: 45.8 MB (52.50%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,48 @@
 class Solution {
+
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 0){
-            return new int[]{-1, -1};
+        if (nums.length == 0) {
+            return new int[] { -1, -1 };
         }
-        
-        int left = binarySearch(nums, target, true);
-        if(left == -1){
-            return new int[]{-1, -1};
+        int leftSide = applyBinarySearchLeftSide(nums, target);
+        int rightSide = applyBinarySearchRightSide(nums, target);
+        if (leftSide != -1 && rightSide != -1) {
+            return new int[] { leftSide, rightSide };
         }
-        
-        int right = binarySearch(nums, target, false);
-        return new int[]{left, right};
+        return new int[] { -1, -1 };
     }
-    
-    private int binarySearch(int[] nums, int target, boolean isLeft) {
+
+    private int applyBinarySearchLeftSide(int[] nums, int target) {
         int low = 0, high = nums.length - 1;
-        int result = -1;
-        
-        while(low <= high) {
+        int leftMost = -1;
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            
-            if(nums[mid] == target) {
-                result = mid;
-                if(isLeft) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } else if(nums[mid] > target) {
+            if (nums[mid] == target) {
+                leftMost = mid;
+                high = mid - 1; // because i want to go on lower side
+            } else if (nums[mid] > target) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        
-        return result;
+        return leftMost;
+    }
+
+    private int applyBinarySearchRightSide(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int rightMost = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                rightMost = mid;
+                low = mid + 1; // because i want to go on lower side
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return rightMost;
     }
 }
