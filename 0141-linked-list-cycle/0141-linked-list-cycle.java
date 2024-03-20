da71548d2077eb336a7194b75fcbd42566196a1e
Nishank Soni <soninishank8@gmail.com>
2024-03-19 21:03:10 -0700

    Time: 0 ms (100.00%), Space: 44.1 MB (84.52%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,11 +10,13 @@
  * }
  */
 public class Solution {
+
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        
-        while(fast != null && fast.next != null){
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             if(slow == fast){
