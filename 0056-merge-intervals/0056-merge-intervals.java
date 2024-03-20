9f0615055be959bcc3df3d7a37f40afff8c3d1dc
Nishank Soni <soninishank8@gmail.com>
2024-03-19 17:02:23 -0700

    Time: 9 ms (56.79%), Space: 46.6 MB (26.08%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,22 +1,23 @@
 class Solution {
 
     public int[][] merge(int[][] intervals) {
+        // Base case
         if (intervals.length == 0) {
             return null;
         }
-        List<int[]> list = new ArrayList<>();
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-        int[] temp = intervals[0];
+        List<int[]> list = new ArrayList<>();
+        int[] currentInterval = intervals[0];
         for (int i = 1; i < intervals.length; i++) {
-            if (temp[1] >= intervals[i][0]) {
-                temp[0] = Math.min(temp[0], intervals[i][0]);
-                temp[1] = Math.max(temp[1], intervals[i][1]);
+            if (intervals[i][0] <= currentInterval[1]) {
+                currentInterval[0] = Math.min(currentInterval[0], intervals[i][0]);
+                currentInterval[1] = Math.max(currentInterval[1], intervals[i][1]);
             } else {
-                list.add(temp);
-                temp = intervals[i];
+                list.add(currentInterval);
+                currentInterval = intervals[i];
             }
         }
-        list.add(temp);
+        list.add(currentInterval);
         return list.toArray(new int[list.size()][]);
     }
 }
