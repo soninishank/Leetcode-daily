8c415716718494cec5dded565bb2b73237c57e6f
Nishank Soni <soninishank8@gmail.com>
2024-03-21 12:46:50 -0700

    Time: 2711 ms (5.49%), Space: 40.4 MB (36.38%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int numTrees(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return solve(1, n);
+    }
+
+    private int solve(int start, int end) {
+        int count = 0;
+        if (start > end) {
+            return 1;
+        }
+        if (start == end) {
+            return 1;
+        }
+        for (int i = start; i <= end; i++) {
+            int leftCount = solve(start, i - 1);
+            int rightCount = solve(i + 1, end);
+
+            count += leftCount * rightCount;
+        }
+        return count;
+    }
+}
